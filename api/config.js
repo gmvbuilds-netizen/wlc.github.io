3b2569cd996d7a5bb15f48678858f2fef4d21ab7
Gmvprtl Server <gmvbuilds@gmail.com>
2026-04-27 21:11:17 +0200

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,20 @@
+const config = {
+  "packName": "Waleriic",
+  "version": "1.0",
+  "downloadUrl": "https://github.com/gmvbuilds-netizen/blackwlf-tp-ad-skip/releases/download/wlc_pack_v1/Waleric.v1.zip",
+  "updateUrl": "https://github.com/gmvbuilds-netizen/blackwlf-tp-ad-skip/releases",
+  "description": "Das offizielle 1.21.11 Texturepack von Waleric mit Custom Hotbar UI und Custom Totem",
+  "settingsPassword": "W4l3r1c_T3xtur3_P4ck_2026_S3cur3_Adm1n!#$%^&*()",
+  "features": [
+    "1.21.11 Support",
+    "Custom Hotbar UI",
+    "Custom Totem of Animating",
+    "Custom Items",
+    "Kostenloser Download"
+  ],
+  "screenshots": [
+    "Not Set up yet",
+    "Not set up yet",
+    "Not set up yet"
+  ]
+};
